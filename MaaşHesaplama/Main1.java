62a575bc0e44dc184a1da45bd6a683fc7a8f82a8
volkanselen <115782304+volkanselen@users.noreply.github.com>
2022-11-14 23:04:04 +0300

    Create Main1.java

---
+++ MaaşHesaplama/Main1.java
@@ -0,0 +1,15 @@
+public class Main1 {
+    public static void main(String[] args){
+
+        Calisan c1= new Calisan("Mehmet",900, 55,2000);
+        Calisan c2= new Calisan("Ahmet",1500, 35,2005);
+        Calisan c3= new Calisan("Zeynep",800, 42,2022);
+        Calisan c4= new Calisan("Berna",2000, 50,2020);
+        c1.tostring();
+
+
+
+
+
+    }
+}
